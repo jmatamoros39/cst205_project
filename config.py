9f1e2ab6f284bb26b773ee70e8c1156270f11012
Anthony G Cervantes <ant.gcervantes@gmail.com>
2025-12-15 00:01:41 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Convert
+API_KEY = "jbhQRLOaLhH2RxyLv0SOchX65KKu0t2x"
+
+# Email
+EMAIL_SERVER = "smtp.gmail.com"
+EMAIL_PORT = 465
+EMAIL_USERNAME = "cst2025.team12@gmail.com"
+EMAIL_PASSWORD = "fxsg njga yjso hwyf"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
